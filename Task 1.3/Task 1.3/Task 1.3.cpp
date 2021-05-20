3b4660e753a5b3be137b9fccd06be6125ab526d5
vlad-seredenko <78535638+vlad-seredenko@users.noreply.github.com>
2021-05-21 00:10:32 +0300

    Add files via upload

---
+++ Task 1.3/Task 1.3/Task 1.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double E;
+    double x;
+    int n = 0;
+    double a = 1.;
+    double  sum = 0;
+    cout << "Введiть Е = ";
+    cin >> E;
+    cout << "Введiть х = ";
+    cin >> x;
+    while (fabs(a) < E)
+    {
+        sum += a;
+        a *= -x / (n + 1.);
+        n++;
+    } 
+ 
+    cout << "(за рядом) y = e^(x) = " << sum << endl;
+    cout << "(через функцiю exp) y = e^(x) = " <<exp(-x)<< endl;
+    return 0;
+}
